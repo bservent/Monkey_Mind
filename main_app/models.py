ad699487a419cdfc94f2d28c0851dc7c3d43ae9d
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-11-30 16:34:09 -0800

    models added and migrated

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Meditation(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.CharField(max_length=100)
+    content = models.TextField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.CharField(max_length=250, default= '')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    login = models.DateField(auto_now_add=True)
+    imageURL = models.ImageField(upload_to = 'profile_image', blank=True, default = '', null = True)
+
+    def __str__(self):
+        return self.name
+        
+     
+
