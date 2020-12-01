50f4e490e00da540aeb30487facd0c69b5342a57
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-11-30 21:15:57 -0800

    div added on home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Meditation, User, Category
+
+
 
 def home(request):
     return render(request, 'home.html')
