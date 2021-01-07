bfbfbe59b45733b835e06f361f7c4435621aa95f
Brittany Servent <bservent@gmail.com>
2021-01-06 21:41:16 -0800

    revert back filing for static files

--- monkeymind_project/settings.py
+++ monkeymind_project/settings.py
@@ -126,8 +126,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
-
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
