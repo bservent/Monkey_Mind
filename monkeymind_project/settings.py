f65b63efe340bb3877b33d1a83f43d5a532513fe
Brittany Servent <bservent@gmail.com>
2021-01-06 21:31:36 -0800

    static files

--- monkeymind_project/settings.py
+++ monkeymind_project/settings.py
@@ -126,9 +126,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_ROOT = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
